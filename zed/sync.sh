1fdb02bf64b7379ba8ae552056134d394871c7e8
danilopereirasilva <danilopereira.contato@proton.me>
2025-03-25 15:34:04 -0300

    atualizaco arquivos zed

--- zed/sync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Use rsync para monitorar e sincronizar os arquivos
-rsync -avz --delete ~/.config/zed/settings.json ~/dotfiles/zed/settings.json &
-
-rsync -avz --delete ~/.config/zed/keymap.json ~/dotfiles/zed/keymap.json &
