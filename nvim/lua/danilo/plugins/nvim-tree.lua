48e7b0f2cdfa624f64d57c61dc76a01280d1de21
danilopereirasilva <danilopereira.contato@proton.me>
2025-10-16 11:02:33 -0300

    feat: removendo features do copilot

--- nvim/lua/danilo/plugins/nvim-tree.lua
+++ nvim/lua/danilo/plugins/nvim-tree.lua
@@ -33,5 +33,25 @@ return {
     vim.keymap.set('n', '<leader>ef', '<cmd>NvimTreeFindFile<CR>', { desc = 'Toggle file explorer on cursor' })
     vim.keymap.set('n', '<leader>ec', '<cmd>NvimTreeCollapse<CR>', { desc = 'Collapse file explorer' })
     vim.keymap.set('n', '<leader>er', '<cmd>NvimTreeRefresh<CR>', { desc = 'Refresh file explorer' })
+
+    -- local function open_nvim_tree(data)
+    --   -- Check if the current buffer is a directory
+    --   local directory = vim.fn.isdirectory(data.file) == 1
+    --   if not directory then
+    --     return
+    --   end
+    --
+    --   -- Change the current working directory to the directory of the file
+    --   vim.cmd.cd(data.file)
+    --
+    --   -- Open nvim-tree in a new window to avoid it occupying the entire window
+    --   require("nvim-tree.api").tree.open({ current_window = false })
+    -- end
+    --
+    -- -- Create the autocmd to trigger the function on VimEnter
+    -- vim.api.nvim_create_autocmd({ "VimEnter" }, {
+    --   callback = open_nvim_tree,
+    --   desc = "Open nvim-tree at startup for directories"
+    -- })
   end
 }
